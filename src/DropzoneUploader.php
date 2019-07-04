a7c3169df6a935b59a0cd8b81ac0e6b49f35f57c
Jakub Mikita <kubomikita@gmail.com>
2019-07-04 13:49:55 +0200

    nette callback invokeArgs deprecated fix

--- src/DropzoneUploader.php
+++ src/DropzoneUploader.php
@@ -245,9 +245,7 @@ class DropzoneUploader extends Nette\Application\UI\Control
 	{
 		if ($event !== null) {
 			foreach ($event as $callback) {
-				// deprecated
-				//Nette\Utils\Callback::invokeArgs($callback, $args);
-				$callback($args);
+				$callback(...$args);
 			}
 		}
 	}
